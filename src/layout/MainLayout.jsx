7fd447886c54d8c564c1e221e2385093aff3590f
Abrarul Rhythm <abrarulrhythm19912006@gmail.com>
2026-09-09 19:06:51 +0600

    setup main wrapper

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,30 @@
 import { Outlet } from 'react-router';
+import Header from '../pages/Shared/Header/Header';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
-        <div>
-            <Outlet></Outlet>
+        // Main Wrapper
+        <div className="main-wrtapper container bg-white border-x border-dark-03">
+            {/* Header */}
+            <header>
+                <Header></Header>
+            </header>
+            {/* Header End */}
+
+            {/* ==================== Mian ==================== */}
+            <main className="site-main">
+                <Outlet></Outlet>
+            </main>
+            {/* ==================== Mian End ==================== */}
+
+            {/* Footer */}
+            <footer>
+                <Footer></Footer>
+            </footer>
+            {/* Footer End */}
         </div>
+        // Main Wrapper End
     );
 };
 
